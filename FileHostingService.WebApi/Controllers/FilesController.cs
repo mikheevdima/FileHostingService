cf9acd44a81434e98261eb51c99d72b8dbb643f8
Dmitrii <micheev.dima@gmail.com>
2017-05-25 02:43:55 +0300

    added files, users controllers

--- FileHostingService.WebApi/Controllers/FilesController.cs
+++ FileHostingService.WebApi/Controllers/FilesController.cs
@@ -9,40 +9,44 @@ using FileHostingService.Model;
 
 namespace FileHostingService.WebApi.Controllers
 {
-    public class UsersController: ApiController
+    public class FilesController : ApiController
     {
         private const string ConnectionString = "Data Source=LENOVO-PC; Initial Catalog=FileSharingDB; Integrated Security=True; Pooling=False";
         private readonly IUsersRepository _usersRepository = new UsersRepository(ConnectionString);
         private readonly IFilesRepository _filesRepository;
 
-        public UsersController()
+        public FilesController()
         {
             _filesRepository = new FilesRepository(_usersRepository, ConnectionString);
         }
 
+        /// <summary> create file </summary>
+        /// <param name="file"> file for creation </param>
+        /// <returns> file </returns>
         [HttpPost]
-        public User CreateUser([FromBody]User user)
+        public File CreateFile(File file)
         {
-            return _usersRepository.Add(user);
+            return _filesRepository.Add(file);
         }
 
+        /// <summary> delete file </summary>
+        /// <param name="id"> file id </param>
+        /// <returns></returns>
         [HttpDelete]
-        public void DeleteUser(Guid id)
+        public void DeleteFile(Guid id)
         {
-            _usersRepository.Delete(id);
+            _filesRepository.Delete(id);
         }
 
+        /// <summary> get file info </summary>
+        /// <param name="id"> file id </param>
+        /// <returns> file </returns>
         [HttpGet]
-        public User GetUser(Guid id)
+        public File GetFileInfo(Guid id)
         {
-            return _usersRepository.Get(id);
+            return _filesRepository.GetInfo(id);
         }
 
-        [Route("api/users/{id}/files")]
-        [HttpGet]
-        public IEnumerable<File> GetUserFiles(Guid id)
-        {
-            return _filesRepository.GetUserFiles(id);
-        }
+
     }
 }
